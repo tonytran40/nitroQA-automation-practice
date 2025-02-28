79cd096a216474234a162e51a74b207f252258ef
Mike <mike.clancy.work@gmail.com>
2025-02-28 13:59:22 -0500

    fix session file path

--- CONSTANTS.ts
+++ CONSTANTS.ts
@@ -3,5 +3,4 @@ import path from "path";
 
 export const NITRO_QA_URL = "https://nitroqa.powerhrg.com";
 export const NITRO_ID_LOGIN = "https://id.powerhrg.com/login";
-const ROOT_DIR = path.resolve(__dirname, '..');
-export const SESSION_FILE = path.resolve(ROOT_DIR, "session.json");
+export const SESSION_FILE = path.resolve(__dirname, "session.json");
